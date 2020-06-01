654d3a7f45ed4bbd9547bc22f72c9545adb9259b
techakademi <66215655+techakademi@users.noreply.github.com>
2020-06-01 10:35:54 +0300

    Update CentosKapat.bat

--- CentosKapat.bat
+++ CentosKapat.bat
@@ -1,4 +1,10 @@
 @echo off
+echo ====================================================
+echo Sanal Makine Kapatiliyor, lutfen bekleyiniz
 cd /d "C:\Program Files\Oracle\VirtualBox"
+echo ====================================================
+TIMEOUT 3
 VBoxManage controlvm "Centos7" acpipowerbutton
+echo Sanal Makine Basarili bir Sekilde Kapatildi
+echo ====================================================
 pause

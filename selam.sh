e26773f100b06b461069acf03b3556362e1d78e4
techakademi <66215655+techakademi@users.noreply.github.com>
2020-06-06 15:31:34 +0300

    Create selam.sh

---
+++ selam.sh
@@ -0,0 +1,15 @@
+#!/bin/sh                                                                                                       
+Bugun=$(date '+%d-%m-%Y');                                                                                      
+Saat=$(date '+%T');                                                                                             echo "-----------------------"                                                                                  
+echo "|Merhaba Ben EchoDroid|"                                                                                  
+echo "-----------------------"                                                                                  
+echo -n "Merhaba Isiminiz ?"                                                                                    
+read isminiz                                                                                                    
+echo &&                                                                                                         
+echo "Memnun Oldum, $isminiz"                                                                                   
+echo &&                                                                                                         
+echo "Seninle Tanistigimiz Tarih:${Bugun},Saat:${Saat}"                                                         
+echo &&                                                                                                         
+read -t 10 -n 1 -s -r -p "On saniye Sonra kendim cikacagim, veya sen herhangi bir tusa basarak beni kapatabilirisin." 
+echo &&                                                                                                         
+echo "Gule Gule, $isminiz :)"

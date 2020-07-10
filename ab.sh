02d77fb208633a8b295bdc247785355516c82832
techakademi <66215655+techakademi@users.noreply.github.com>
2020-07-10 18:24:31 +0300

    Update ab.sh

--- ab.sh
+++ ab.sh
@@ -1,3 +1,7 @@
+#!/bin/bash                                                                                                       
+#Bugun=$(date '+%d-%m-%Y');   
+Bugun=$(date '+%d-%m-%Y');                                                                                     
+Saat=$(date '+%H:%M');      
 echo ""
 echo -e "\e[31m----------------------------\e[0m"                                                      
 echo -e "\e[1m|(:..:)LB Tester(:..:)|\e[0m"                                                      
@@ -10,7 +14,7 @@ echo ""
     while true; do
 	    read -p "Bu scripti kullanma sorumluluğunu kabul ediyor musun ? Ee/Hh : " eh 
 	    case $eh in
-	        [Ee]* ) echo "Tebrikler" echo ""
+	        [Ee]* )
     echo ""
     echo "Test Adresini www.deneme.com, veya ip ve port adresi 127.0.0.1:81 şeklinde girmelisin." &&
     echo -n "Url: "
@@ -27,11 +31,14 @@ echo ""
     echo "Test hazırlıklarına başlıyorum." &&
     echo ""
     sleep 2
-    ab -n $REQ -c $CON http://$URL/
+    ab -n $REQ -c $CON http://$URL/ 2>&1  | tee /home/$URL-$Bugun-$Saat.txt
+    sleep 1
     echo ""
-    echo "Umarım sonuçlar tatmin edicidir :), Hoşçakal"; break;;
+    echo "Ciktilari /home/ klasörü altına $URL-$Bugun-$Saat.txt adıyla kaydettim."
+    sleep 3
+    echo "Umarım sonuçlar tatmin edicidir :), Hoşçakal."; break;;
 
-            [Hh]* ) echo "Umarım doğru bir karar vermişsindir...";  exit;;
+            [Hh]* ) echo "Kolay gelsin görşmek üzere...";  exit;;
 	         * ) 	
 	    esac
     done

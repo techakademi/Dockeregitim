9df84da7ffd5a24985daa315b53cb578b8bc382c
techakademi <66215655+techakademi@users.noreply.github.com>
2020-12-28 13:31:45 +0300

    Update kontdroid.sh

--- TümScriptler/kontdroid.sh
+++ TümScriptler/kontdroid.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 echo ""
 echo -e "\e[31m----------------------------\e[0m"    
 echo -e "\e[1m|(:..:)KontExternator(:..:)|\e[0m"                                                                                                       
